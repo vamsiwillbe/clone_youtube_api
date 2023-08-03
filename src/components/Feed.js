b2a9fe06666a36f2c9144f4267e60d3b39b16b8e
Lavanya Prakash jampana <vamsiwillb@gmail.com>
2023-08-03 20:41:01 +0530

    first commit

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -21,7 +21,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          © Harsha Sai
+          © Vamsi_will_be
         </Typography>
       </Box>
 
